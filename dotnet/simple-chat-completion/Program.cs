6ecc7e68d30c1fb7ff4271836fa7168ffdd69824
Saeed Motamed <saeedmotamed@hotmail.com>
2024-12-21 22:59:39 +0400

    simple-chat-completion Add

---
+++ dotnet/simple-chat-completion/Program.cs
@@ -0,0 +1,74 @@
+﻿// Import packages
+
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.ChatCompletion;
+using Microsoft.SemanticKernel.Connectors.Ollama;
+
+
+#pragma warning disable format
+#pragma warning disable CA1861
+#pragma warning disable SKEXP0070
+#pragma warning disable SKEXP0001
+#pragma warning disable CS8604
+#pragma warning disable SKEXP0010
+
+internal class Program
+{
+    private static async Task Main(string[] args)
+    {
+        // Set the ollama url and model variables
+        var url = new Uri("http://localhost:11434");
+        var model = "llama3.1:latest";
+
+        // Create a new kernel builder
+        var builder = Kernel.CreateBuilder();
+
+        // Add the ollama services to the kernel
+        builder.AddOllamaChatCompletion(model, url, "ollamaChat");
+
+        // Build the kernel
+        Kernel kernel = builder.Build();
+
+        // Get the chat service
+        var chatService = kernel.GetRequiredService<IChatCompletionService>();
+
+        // Set the Planning and Chat Settings Parameters
+        OllamaPromptExecutionSettings ollamaPromptSettings = new OllamaPromptExecutionSettings();
+        ollamaPromptSettings.FunctionChoiceBehavior = FunctionChoiceBehavior.Auto();
+        ollamaPromptSettings.Temperature = 0.7f;
+        ollamaPromptSettings.TopP = 0.95f;
+
+       // create an object of ChatHistory to track the conversation
+        var history = new ChatHistory();
+
+        // Add a System message
+        history.AddSystemMessage("You are an AI assistant that helps people find information.");
+  
+
+        // ====================== Start the chat loop ======================
+        string? userInput;
+        do
+        {
+            // Collect user input
+            Console.Write("User > ");
+            userInput = Console.ReadLine();
+
+            // Add user input
+            history.AddUserMessage(userInput);
+
+            // Get the response from the AI
+            var result = await chatService.GetChatMessageContentAsync(
+                history,
+                executionSettings: ollamaPromptSettings,
+                kernel: kernel);
+
+            // Print the results
+            Console.WriteLine("Assistant > " + result);
+
+            // Add the message from the agent to the chat history
+            history.AddAssistantMessage(result.ToString());
+            Console.WriteLine(result.ToString());
+        } while (userInput is not null);
+
+    }
+}
